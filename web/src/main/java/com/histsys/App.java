cbe8900b1457f10f93274623e4c992d081f78df4
cocode <seeucoco@163.com>
2021-07-01 19:36:59 +0800

    fix workflow

--- web/src/main/java/com/histsys/App.java
+++ web/src/main/java/com/histsys/App.java
@@ -3,10 +3,15 @@ package com.histsys;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+//        SpringApplication app = new SpringApplication(App.class);
+//        app.setDefaultProperties(Collections .singletonMap("server.port", "8083"));
+//        app.run(args);
     }
 }
